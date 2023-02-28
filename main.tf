d650e8b3b023019a23b07451d040670b438b252f
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-02-28 16:26:52 +0100

    Fixing main parameters definition

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
 resource "azurerm_cdn_frontdoor_origin_group" "this" {
-  name                     = "example-origin-group"
-  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.example.id
-  session_affinity_enabled = true
+  name                     = var.name
+  cdn_frontdoor_profile_id = var.cdn_frontdoor_profile_id
+  session_affinity_enabled = var.session_affinity_enabled
 
-  restore_traffic_time_to_healed_or_new_endpoint_in_minutes = 10
-  session_affinity_enabled - (Optional) Specifies whether session affinity should be enabled on this host. Defaults to true.
+  restore_traffic_time_to_healed_or_new_endpoint_in_minutes = var.restore_traffic_time_to_healed_or_new_endpoint_in_minutes
 
    health_probe {
             interval_in_seconds = var.health_probe_interval_in_seconds
