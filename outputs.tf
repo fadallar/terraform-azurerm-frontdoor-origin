c1d25625f3c36fc42d1d16895d4153ccc5872592
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-03-14 20:25:13 +0100

    Updating outputs

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,9 @@ output "origin_group_id" {
   value       = azurerm_cdn_frontdoor_origin_group.this.id
 }
 
-#output "origin_id_list" {
-#  description = "List of FrontDoor Origin Ids"
-#  value = [
-#    for origin in azure_cdn_frontdoor_origin.this : origin.id
-#  ]
-#}
+output "origin_id_list" {
+  description = "List of FrontDoor Origin Ids"
+  value = tomap({
+    for k, origin in azure_cdn_frontdoor_origin.this : k => origin.id
+  })
+}
