a7b967567937b04350caf5dd6fdfe500128c9170
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-03-15 05:56:29 +0100

    Working on outputs

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,5 @@ output "origin_group_id" {
 
 output "origin_id_list" {
   description = "List of FrontDoor Origin Ids"
-  value = tomap({
-    for k, origin in azure_cdn_frontdoor_origin.this : k => origin.id
-  })
+  value = { for origin in azure_cdn_frontdoor_origin.this : origin.name => origin.id }
 }
